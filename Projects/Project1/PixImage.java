908a98c00d27b53c6b62b9563806949c1688630e
woshizhpy <2403247037@qq.com>
2017-04-15 23:51:31 +0800

    Update for Project1 04/15/2017

--- Projects/Project1/PixImage.java
+++ Projects/Project1/PixImage.java
@@ -14,6 +14,7 @@
  *  All methods in this class must be implemented to complete Part I.
  *  See the README file accompanying this project for additional details.
  */
+import java.lang.*;
 
 public class PixImage {
 
@@ -21,7 +22,13 @@ public class PixImage {
    *  Define any variables associated with a PixImage object here.  These
    *  variables MUST be private.
    */
+   private int width;
+   private int height;
+   private final int total=3; 
+   //private fianl int red=0,green=1,blue=2;
 
+   private short[][][] Pixel_Value;
+   // Pixel_Value[x][y][0] Red; [1] Green; [2] Blue
 
 
 
@@ -34,6 +41,14 @@ public class PixImage {
    */
   public PixImage(int width, int height) {
     // Your solution here.
+	this.width=width;
+	this.height=height;
+	Pixel_Value= new short[width][height][total];
+  }
+  public PixImage()
+  {
+	  width=0;
+	  height=0;
   }
 
   /**
@@ -43,7 +58,7 @@ public class PixImage {
    */
   public int getWidth() {
     // Replace the following line with your solution.
-    return 1;
+	return width;
   }
 
   /**
@@ -53,7 +68,8 @@ public class PixImage {
    */
   public int getHeight() {
     // Replace the following line with your solution.
-    return 1;
+   //return 1;
+   return height;
   }
 
   /**
@@ -65,7 +81,8 @@ public class PixImage {
    */
   public short getRed(int x, int y) {
     // Replace the following line with your solution.
-    return 0;
+    //return 0;
+	return Pixel_Value[x][y][0];
   }
 
   /**
@@ -77,7 +94,8 @@ public class PixImage {
    */
   public short getGreen(int x, int y) {
     // Replace the following line with your solution.
-    return 0;
+    //return 0;
+	return Pixel_Value[x][y][1];
   }
 
   /**
@@ -89,7 +107,8 @@ public class PixImage {
    */
   public short getBlue(int x, int y) {
     // Replace the following line with your solution.
-    return 0;
+    //return 0;
+	return Pixel_Value[x][y][2];
   }
 
   /**
@@ -107,6 +126,11 @@ public class PixImage {
    */
   public void setPixel(int x, int y, short red, short green, short blue) {
     // Your solution here.
+	//System.out.println(x+" "+y);
+	Pixel_Value[x][y][0]=red;
+	Pixel_Value[x][y][1]=green;
+	Pixel_Value[x][y][2]=blue;
+	//System.out.println(x+" "+y+" "+Pixel_Value[x][y][0]+" "+Pixel_Value[x][y][1]+" "+Pixel_Value[x][y][2]);
   }
 
   /**
@@ -120,7 +144,37 @@ public class PixImage {
    */
   public String toString() {
     // Replace the following line with your solution.
-    return "";
+	//String s=new String("Now show the PixelImage:\r\n");
+	String s=new String();
+	s+=ColortoString(0);
+	//s+="Green_Value:\r\n"+ColortoString(1);
+	//s+="Blue_Value:\r\n"+ColortoString(2);
+    return s;
+  }
+  public String ColortoString(int color)
+  {
+	  String s=new String("\r\n-----");
+	  for(int k=0;k<height;k++)
+	  {
+		  s+="----";
+	  }
+	  s+="\r\n";
+	  for(int j=0;j<height;j++)
+	  {
+		  for(int i=0;i<width;i++)
+		  {
+			  s+="| "+Short.toString(Pixel_Value[i][j][color]);
+			  
+		  }
+		  s+="|\r\n";
+	  }
+	  s+="-----";
+	  for(int k=0;k<height;k++)
+	  {
+		  s+="----";
+	  }
+	  s+="\r\n";
+	  return s;
   }
 
   /**
@@ -152,9 +206,171 @@ public class PixImage {
    * @param numIterations the number of iterations of box blurring.
    * @return a blurred version of "this" PixImage.
    */
+   public int[] GetPixelType (int x,int y) {
+	   int[] PixelType=new int[3];
+	   // Identify corner or side
+	  //System.out.println("This is in GetPixelType,x="+x+",y="+y);
+		if(x==0)// left corner
+		{
+			if(y==0)
+			{	
+				PixelType[0]=4;
+				PixelType[1]=0;// x start coordinate
+				PixelType[2]=0;// y start coordinate
+				return PixelType; 
+			}
+			else if(y==height-1)
+			{
+				PixelType[0]=4;
+				PixelType[1]=0;// x start coordinate
+				PixelType[2]=-1;// y start coordinate
+				return PixelType;
+			}
+			else //left side
+			{
+				PixelType[0]=6;
+				PixelType[1]=1;// left so y has a range of 3
+				PixelType[2]=0;// x start coordinate
+				return PixelType;
+			}
+		}
+		else if(x==width-1)
+		{
+			if(y==0)
+			{
+				PixelType[0]=4;
+				PixelType[1]=-1;// x start coordinate
+				PixelType[2]=0;// y start coordinate
+				return PixelType;
+			}
+			else if(y==height-1)
+			{
+				PixelType[0]=4;
+				PixelType[1]=-1;// x start coordinate
+				PixelType[2]=-1;// y start coordinate
+				return PixelType;
+			}
+			else //left side
+			{
+				PixelType[0]=6;
+				PixelType[1]=1;// right so y has a range of 3
+				PixelType[2]=-1;// x start coordinate
+				return PixelType;
+			}
+		}
+		else if(y==0)
+		{
+			PixelType[0]=6;
+			PixelType[1]=2;// up so x has a range of 3
+			PixelType[2]=0;// y start coordinate
+			return PixelType;
+		}
+		else if(y==height-1)
+		{
+			PixelType[0]=6;
+			PixelType[1]=2;// down so x has a range of 3
+			PixelType[2]=-1;// y start coordinate
+			return PixelType;
+		}
+		else
+		{
+			PixelType[0]=9; //normal cases
+			PixelType[1]=0;
+			PixelType[2]=0;
+			return PixelType;
+		}
+				
+	   
+	   
+	   //return PixelType;
+   }
+   public short PixelNebr_Avg(int x,int y,int color) //color:0 for red;1 for green;2 for blue
+   {
+	 int sum=0;
+	 int i,j;
+	 //System.out.println("This is in PixelNebrAvg,x="+x+",y="+y);
+	 int[] PixelType=GetPixelType(x,y);
+	 //System.out.print("After Gettype,x="+x+" y="+y+" Typeis"+PixelType[0]+" "+PixelType[1]+" "+PixelType[2]);
+	 
+	 switch(PixelType[0])
+	 {
+		case 4: //at corners
+			//sum=(short)(Pixel_Value[x][y][color]+Pixel_Value[x+1][y][color]+Pixel_Value[x][y+1][color]+Pixel_Value[x+1][y+1][color]);
+			for(i=x+PixelType[1];i<x+PixelType[1]+2;i++) {
+				for(j=y+PixelType[2];j<y+PixelType[2]+2;j++)
+				{
+					sum+=Pixel_Value[i][j][color];
+				}
+			}
+			break;
+		/* case 42:// the right up corner
+			sum=(short)(Pixel_Value[x][y-1][color]+Pixel_Value[x][y][color]+Pixel_Value[x+1][y-1][color]+Pixel_Value[x+1][y][color]);
+			break;
+		case 43:// the left down corner
+			sum=(short)(Pixel_Value[x][y][color]+Pixel_Value[x][y+1][color]+Pixel_Value[x-1][y][color]+Pixel_Value[x-1][y+1][color]);
+			break;
+		case 44:
+			sum=(short)(Pixel_Value[x][y][color]+Pixel_Value[x-1][y-1][color]+Pixel_Value[x][y-1][color]+Pixel_Value[x-1][y][color]);
+			break; */
+		case 6://at sides Pixel_Type[1]=1 :left or right,Pixel_Type=2: up or down; Pixel_Type[2] is the start of coordinate with a range of 2
+			if(PixelType[1]==1)
+			{
+				for(i=x+PixelType[2];i<x+PixelType[2]+2;i++)
+				{
+					for(j=y-1;j<y+2;j++)
+					{
+						sum+=Pixel_Value[i][j][color];
+					}
+				}
+			}
+			else if(PixelType[1]==2)
+			{
+				for(i=x-1;i<x+2;i++)
+				{
+					for(j=y+PixelType[2];j<y+PixelType[2]+2;j++)
+					{
+						sum+=Pixel_Value[i][j][color];
+					}
+				}
+			}
+			else
+			{
+				System.out.println("Pixel Type Error! It's not a side Pixel!");
+			}
+			break;
+			
+		case 9:
+			for(i=x-1;i<x+2;i++)
+			{
+				for(j=y-1;j<y+2;j++)
+				{
+					sum+=Pixel_Value[i][j][color];
+				}
+			}
+		    break;
+		default:
+			System.out.println("Pixel Type Error!");
+			break;
+	 }
+	 short Avg=(short)(sum/PixelType[0]);
+	 return Avg;
+		
+   }
   public PixImage boxBlur(int numIterations) {
     // Replace the following line with your solution.
-    return this;
+    //return this;
+	 PixImage BlurImg=new PixImage(this.width,this.height);
+	 // boxBlur for red 
+	 int i,j,k;
+	 for(i=0;i<BlurImg.width;i++) {
+		 for(j=0;j<BlurImg.height;j++) {
+			 
+			BlurImg.Pixel_Value[i][j][0]=this.PixelNebr_Avg(i,j,0);
+			BlurImg.Pixel_Value[i][j][1]=this.PixelNebr_Avg(i,j,1);
+			BlurImg.Pixel_Value[i][j][2]=this.PixelNebr_Avg(i,j,2);
+		 }
+	 }
+    return BlurImg;	 
   }
 
   /**
@@ -283,19 +499,26 @@ public class PixImage {
    * and Sobel) are correct.
    */
   public static void main(String[] args) {
+	  String s="I love you"+"\r\n"+"But I love other people as well\r\n";
+	  System.out.print(s);
     // Be forwarned that when you write arrays directly in Java as below,
     // each "row" of text is a column of your image--the numbers get
     // transposed.
+	final int total=3; 
+	int width=3,height=3;
+    short[][][] Pixel_Value= new short[width][height][total];
+	Pixel_Value[0][0][0]=(short) 10;
+	System.out.println(Pixel_Value[0][0][0]);
+	
     PixImage image1 = array2PixImage(new int[][] { { 0, 10, 240 },
                                                    { 30, 120, 250 },
                                                    { 80, 250, 255 } });
     System.out.println("Testing getWidth/getHeight on a 3x3 image.  " +
                        "Input image:");
     System.out.print(image1);
-    doTest(image1.getWidth() == 3 && image1.getHeight() == 3,
-           "Incorrect image width and height.");
+    doTest(image1.getWidth() == 3 && image1.getHeight() == 3,"Incorrect image width and height.");
 
-    System.out.println("Testing blurring on a 3x3 image.");
+     System.out.println("Testing blurring on a 3x3 image.");
     doTest(image1.boxBlur(1).equals(
            array2PixImage(new int[][] { { 40, 108, 155 },
                                         { 81, 137, 187 },
@@ -311,7 +534,7 @@ public class PixImage {
            image1.boxBlur(2) + image1.boxBlur(1).boxBlur(1));
 
     System.out.println("Testing edge detection on a 3x3 image.");
-    doTest(image1.sobelEdges().equals(
+    /* doTest(image1.sobelEdges().equals(
            array2PixImage(new int[][] { { 104, 189, 180 },
                                         { 160, 193, 157 },
                                         { 166, 178, 96 } })),
@@ -336,6 +559,6 @@ public class PixImage {
     doTest(image2.sobelEdges().equals(
            array2PixImage(new int[][] { { 122, 143, 74 },
                                         { 74, 143, 122 } })),
-           "Incorrect Sobel:\n" + image2.sobelEdges());
-  }
+           "Incorrect Sobel:\n" + image2.sobelEdges()); */
+  } 
 }
